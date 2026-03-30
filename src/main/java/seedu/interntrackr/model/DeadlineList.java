f538a8afdc86aa5b12303afba72f60ae5d91ed1f
Chun Nga Lee <leeeugenia16@gmail.com>
2026-03-30 21:23:01 +0800

    fix: Handle empty deadline list in DeadlineListCommand
    DeadlineListCommand assumes that the selected application contains at
    least one deadline.
    
    Applications may not have any deadlines associated with them, which
    leads to unclear or misleading output.
    
    Let's handle the case where the DeadlineList is empty by displaying an
    appropriate message and exiting early.
    
    Providing explicit feedback improves user experience and prevents
    unnecessary iteration over an empty list.

--- src/main/java/seedu/interntrackr/model/DeadlineList.java
+++ src/main/java/seedu/interntrackr/model/DeadlineList.java
@@ -1,10 +1,13 @@
 package seedu.interntrackr.model;
 
 import java.util.ArrayList;
+import java.util.logging.Logger;
 import java.util.Collections;
 import java.util.List;
 
 public class DeadlineList {
+
+    private static final Logger logger = Logger.getLogger(ApplicationList.class.getName());
 
     private ArrayList<Deadline> deadlines;
 
@@ -21,6 +24,7 @@ public class DeadlineList {
      * @param deadlines The preloaded list of deadlines.
      */
     public DeadlineList(ArrayList<Deadline> deadlines) {
+        assert deadlines != null : "Deadlines list cannot be null";
         this.deadlines = deadlines;
     }
 
@@ -30,7 +34,9 @@ public class DeadlineList {
      * @param deadline The deadline to add.
      */
     public void addDeadline(Deadline deadline) {
+        assert deadline != null : "Deadline cannot be null";
         deadlines.add(deadline);
+        logger.fine("Added deadline: " + deadline.getDeadlineType() + " | " + deadline.getDueDate());
     }
 
     /**
